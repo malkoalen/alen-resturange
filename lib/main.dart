e999a2b3ca2e6d2fd9635aac3160df4b1c199801
malkoalen <89979955+malkoalen@users.noreply.github.com>
2024-05-31 19:40:18 +0100

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,43 @@
+import 'package:alenresturange/main.dart';
+import 'package:alenresturange/services/auth/auth_gate.dart';
+import 'package:alenresturange/services/auth/login_or_register.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:alenresturange/models/restaurant.dart';
 import 'package:alenresturange/themes/theme_provider.dart';
 import 'package:alenresturange/auth/login_or_register.dart';
 import 'package:provider/provider.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform);
 
   runApp(
-    MultiProvider(providers: [
-      // theme provider 
-      ChangeNotifierProvider(create: (context) => ThemProvider()),
+    MultiProvider(
+      providers: [
+        // theme provider
+        ChangeNotifierProvider(create: (context) => ThemProvider()),
 
-      // resturant provider
-      ChangeNotifierProvider(create: (context) => Restaurant()),
-    ],
-    child: const MyApp(),
+        // restaurant provider
+        ChangeNotifierProvider(create: (context) => Restaurant()),
+      ],
+      child: const MyApp(),
     ),
   );
 }
+
+mixin currentPlatform {
+}
+
+mixin currentplatform {
+}
+
+class DefaultFirebaseOptions {
+  static var currentPlatform;
+}
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -27,7 +46,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const LoginOrRegister(),
+      home: const AuthGate(),
       theme: Provider.of<ThemProvider>(context).themeData,
     );
   }
